3f1e9a011dc518d77e1015171bce301bfe74dc60
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-09 09:49:52 +0530

    Updated page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -82,7 +82,7 @@ export default function Home() {
                   name="handle"
                   value={handle}
                   onChange={onChange}
-                  placeholder="ujjwal tiwari"
+                  placeholder="ujjwaltiwari"
                   className="px-3 py-2 rounded-xl bg-white/5 border border-white/10 outline-none focus:ring-2 ring-cyan-300/50"
                 />
               </div>
@@ -112,7 +112,7 @@ export default function Home() {
                   name="bio"
                   value={data.bio}
                   onChange={onChange}
-                  placeholder="I build fast, reliable services and love distributed systems."
+                  placeholder="I build fast, reliable services "
                   className="px-3 py-2 rounded-xl bg-white/5 border border-white/10 outline-none focus:ring-2 ring-cyan-300/50 min-h-[88px]"
                 />
               </div>
