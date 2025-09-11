75a89b9c2d8635baaeafc8e36cb3b42ff33f89f7
Ujjwal Tiwari <ujjwal02tiwari@gmail.com>
2025-09-11 17:50:11 +0530

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,18 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-// Reuse a single client in dev to avoid "too many connections" on HMR
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
-const prisma =
+export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    // log: ["query", "error", "warn"], // optional
+    log:
+      process.env.NODE_ENV === "development"
+        ? ["query", "error", "warn"]
+        : ["error"],
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
+
 
